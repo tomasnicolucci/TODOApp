579b2e729c2899e0cdf678fa27f5296d03a916f5
tomasnicolucci <tomas.nicolucci@gmail.com>
2023-12-06 01:12:12 -0300

    Add routes

---
+++ Backend/routes/todoItem.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/todoItem');
+
+router.get('/', async(req,res) => {
+    res.json(await controller.getTodoItems());
+})
+
+router.post('/', async(req,res) => {
+    res.json(await controller.newTodoItem(req.body));
+})
+
+router.delete('/:id', async(req,res) => {
+    res.json(await controller.deleteTodoItem(req.params.id));
+})
+
+router.put('/:id', async(req,res) => {
+    res.json(await controller.putTodoItem(req.params.id, req.body));
+})
+
+module.exports = router;
